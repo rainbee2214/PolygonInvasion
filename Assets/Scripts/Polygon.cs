bb201f80b964045e53d046266dd444ce434b13c8
rainbee2214 <rainbee2214@gmail.com>
2014-11-23 14:19:38 -0400

    Initial project. Attack of the polygons.

---
+++ Assets/Scripts/Polygon.cs
@@ -0,0 +1,246 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Polygon : MonoBehaviour 
+{
+	static Vector2 OUT_OF_VIEW = new Vector2(1000f, 1000f);
+	[Range(-100f, 100f)]
+	public float currentRotationSpeed;
+	[Range(-0.99f, 0.99f)]
+	public float currentVelocity;
+	public float currentDelay;
+
+	public Material[] materials;
+	public Color[] colors;
+	public Color currentColor;
+	public Material currentMaterial;
+	public string currentDirection;
+
+	public float hVelocity;
+	public float vVelocity;
+	public Vector2 currentPosition;
+	public float startTime;
+
+	public bool frozen = true;
+
+	public float maxHealth;
+	public float currentHealth;
+
+	float lastVelocity;
+	bool changeMaterial;
+
+	void Start()
+	{
+		SetMaterials();
+	}
+	//Defaults
+	public void ConfigurePolygon()
+	{
+		ConfigurePolygon("line", "right", "red", 0.1f, 1f, 100f);
+	}
+
+	public void ConfigurePolygon(float delay)
+	{
+		ConfigurePolygon("line", "right", "red", 0.15f, delay, 100f);
+	}
+
+	public void ConfigurePolygon(string material, string direction, string color, float velocity, float delay, float rotation)
+	{
+		SetMaterials();
+		startTime = Time.time;
+		SetHealth();
+		SetDelay(delay);
+		SetColor(color);
+		SetMaterial(material);
+		SetVelocity(velocity);
+		SetDirection(direction);
+		SetRotationSpeed(rotation);
+		currentPosition = transform.parent.position;
+	}
+
+	void Update () 
+	{
+		//SetMaterial("line");
+
+		if (changeMaterial) 
+		{
+			Material[] temp = gameObject.renderer.materials;
+			temp[0] = currentMaterial;
+			gameObject.renderer.materials = temp;
+
+			changeMaterial = false;
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (Time.time > startTime + currentDelay && !frozen)
+		{
+			transform.Rotate(Vector3.forward * Time.deltaTime * currentRotationSpeed);
+			if (lastVelocity != currentVelocity) SetDirection(currentDirection);
+			currentPosition += new Vector2(hVelocity, vVelocity);
+			gameObject.transform.position = currentPosition;
+		}
+		else
+		{
+			gameObject.transform.position = currentPosition;
+		}
+	}
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.tag == "Buffer")
+		{
+			SetDirection(collision.name);
+		}
+	}
+	
+	public void SetColor(string color)
+	{
+		switch(color)
+		{
+		case "Red": case "red": currentColor = colors[0]; break;
+		case "Orange": case "orange": currentColor = colors[1]; break;
+		case "Yellow": case "yellow": currentColor = colors[2]; break;
+		case "Green": case "green": currentColor = colors[3]; break;
+		case "Indigo": case "indigo": currentColor = colors[4]; break;
+		case "Violet": case "violet": currentColor = colors[5]; break;
+		case "Purple": case "purple": currentColor = colors[6]; break;
+		case "Pink": case "pink": currentColor = colors[7]; break;
+		default: break;
+		}
+	}
+
+	public void SetMaterial(string material)
+	{
+		switch(material)
+		{
+		case "Line": case "line": currentMaterial = materials[0]; break;
+		case "Triangle": case "triangle": currentMaterial = materials[1]; break;
+		case "Square": case "square": currentMaterial = materials[2]; break;
+		case "Pentagon": case "pentagon": currentMaterial = materials[3]; break;
+		case "Hexagon": case "hexagon": currentMaterial = materials[4]; break;
+		case "Heptagon": case "heptagon": currentMaterial = materials[5]; break;
+		case "Octagon": case "octagon": currentMaterial = materials[6]; break;
+		case "Circle": case "circle": currentMaterial = materials[7]; break;
+		default: break;
+		}
+
+		changeMaterial = true;
+	}
+
+	public void SetMaterial (int materialIndex)
+	{
+		currentMaterial = materials[materialIndex];
+
+		changeMaterial = true;
+	}
+
+	public void SetDirection(string direction)
+	{
+		switch(direction)
+		{
+		case "right":
+		{
+			vVelocity = 0f;
+			hVelocity = currentVelocity;
+			break;
+		}
+		case "left":
+		{
+			vVelocity = 0f;
+			hVelocity = -currentVelocity;
+			break;
+		}
+		case "up":
+		{
+			hVelocity = 0f;
+			vVelocity = currentVelocity;
+			break;
+		}
+		case "down":
+		{
+			hVelocity = 0f;
+			vVelocity = -currentVelocity;
+			break;
+		}
+		default: break;
+		}
+		lastVelocity = currentVelocity;
+	}
+
+	void SetMaterials()
+	{
+		materials = new Material[8];
+		materials[0] = Instantiate (Resources.Load("Materials/Line", typeof(Material))) as Material;
+		materials[1] = Instantiate (Resources.Load("Materials/Triangle", typeof(Material))) as Material;
+		materials[2] = Instantiate (Resources.Load("Materials/Square", typeof(Material))) as Material;
+		materials[3] = Instantiate (Resources.Load("Materials/Pentagon", typeof(Material))) as Material;
+		materials[4] = Instantiate (Resources.Load("Materials/Hexagon", typeof(Material))) as Material;
+		materials[5] = Instantiate (Resources.Load("Materials/Heptagon", typeof(Material))) as Material;
+		materials[6] = Instantiate (Resources.Load("Materials/Octagon", typeof(Material))) as Material;
+		materials[7] = Instantiate (Resources.Load("Materials/Circle", typeof(Material))) as Material;
+	}
+	
+	public void SetColor(int colorIndex)
+	{
+		if (colorIndex < colors.Length)currentColor = colors[0];
+	}
+	
+	public void SetColor(Color color)
+	{
+		currentColor = color;
+	}
+	
+	public string GetDirection()
+	{
+		return currentDirection;
+	}
+
+	public void FreezePolygon()
+	{
+		frozen = true;
+	}
+	
+	public void UnFreezePolygon()
+	{
+		frozen = false;
+	}
+	
+	public void ResetPolygon()
+	{
+		currentPosition = OUT_OF_VIEW;
+		frozen = true;
+	}
+	
+	public void SetRotationSpeed(float rotationSpeed)
+	{
+		currentRotationSpeed = rotationSpeed;
+	}
+	
+	public void SetHealth()
+	{
+		currentHealth = maxHealth;
+	}
+	
+	public void SetDelay(float delay)
+	{
+		currentDelay = delay;
+	}
+	
+	public float GetDelay()
+	{
+		return currentDelay;
+	}
+	
+	public void SetVelocity(float velocity)
+	{
+		currentVelocity = velocity;
+	}
+	
+	public float GetVelocity()
+	{
+		return currentVelocity;
+	}
+	
+}
---
+++ Assets/Scripts/PolygonEmitter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PolygonEmitter : MonoBehaviour 
+{
+	List<GameObject> polygonPool;
+	int poolSize = 80;
+
+	void Start () 
+	{
+		CreatePolygonPool();	
+	}
+	
+	void Update () 
+	{
+		
+	}	
+
+	void CreatePolygonPool()
+	{
+		polygonPool = new List<GameObject>();
+		for (int i = 0; i < poolSize; i++)
+		{
+			polygonPool.Add(Instantiate(Resources.Load("Prefabs/Polygon", typeof(GameObject))) as GameObject);
+		}
+		for (int i = 0; i < poolSize; i++)
+		{
+			polygonPool[i].transform.parent = transform;
+			polygonPool[i].gameObject.name = "Polygon"+i;
+			polygonPool[i].gameObject.GetComponent<Polygon>().ConfigurePolygon(i*0.25f);
+			polygonPool[i].gameObject.GetComponent<Polygon>().UnFreezePolygon();
+		}
+		int index = 0;
+		for (int i = 0; i < poolSize; i++)
+		{
+			if (i%10 == 0 && i != 0) index++;
+			if (index >= polygonPool[i].gameObject.GetComponent<Polygon>().materials.Length) index = 0;
+			polygonPool[i].gameObject.GetComponent<Polygon>().SetMaterial(index);
+		}
+	}
+}
